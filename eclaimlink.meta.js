0d2f2f40a64d8b8b542c5ab027a3a2024ec3682a
Abdo Khaire <ahmed.abdo3@hotmail.com>
2023-10-10 12:30:38 +0400

    Create eclaimlink.meta.js

---
+++ eclaimlink.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         eclaimlink.ae Export to CSV Script
+// @namespace    https://github.com/abdokhaire/eclaimlink.ae-Script
+// @version      0.1
+// @description  This Script will add two buttons to the transaction list which will allow you extract CSV file with all the data required
+// @author       You
+// @match        https://*.eclaimlink.ae/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=hayageek.com
+// @grant        unsafeWindow
+// @updateURL   https://raw.githubusercontent.com/abdokhaire/eclaimlink.ae-Script/main/eclaimlink.meta.js
+// @downloadURL https://raw.githubusercontent.com/abdokhaire/eclaimlink.ae-Script/main/eclaimlink.user.js
+// @supportURL  https://github.com/abdokhaire/eclaimlink.ae-Script/issues
+// @author      Ahmed Abdo
+
+// ==/UserScript==
